758008e2ecef1577120df6c83fc0debdbf13b5f5
jumagoca78 <jumagoca78@hotmail.com>
2020-01-10 09:27:42 -0600

    This the code for the Java Triangle

---
+++ JavaTriangle/src/Triangle.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JuanManuel
+ */
+public class Triangle {
+    
+    public String checkTriangle(int a, int b, int c){
+        
+        return "El triangulo es: ";
+    }
+    
+}
